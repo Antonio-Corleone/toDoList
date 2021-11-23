2cc7d227e80af56546978c2cd923b13c4bf07a92
Antonio-Corleone <vuminhtuan3795@gmail.com>
2021-11-23 14:44:30 +0700

    Added getListTask function and render data

--- js/main.js
+++ js/main.js
@@ -1,2 +1,56 @@
 import Task from "./Task.js";
 import TaskService from "./TaskService.js";
+const taskService = new TaskService();
+
+const getELE = (id) => document.getElementById(id);
+
+// Get list tasks from API
+const getTaskApi = () => {
+  taskService.getListTasks()
+    .then((result) => {
+      console.log(result);
+      renderData(result.data);
+    })
+    .catch((error) => console.log(error))
+}
+getTaskApi();
+
+//Render data
+const renderData = (data) => {
+  let completed = '';
+  let todo = '';
+  data.forEach((task) => {
+    if (task.status === 'completed') {
+      completed += 
+      `<li>
+        <span>${task.textTask}</span>
+        <div class="buttons">
+          <button class="remove">
+            <i class="fa fa-trash-alt"></i>
+          </button>
+          <button class="complete">
+            <i class="far fa-check-circle"></i>
+            <i class="fas fa-check-circle"></i>
+          </button>
+        </div>
+      </li>`
+    }
+    else {
+      todo += 
+      `<li>
+        <span>${task.textTask}</span>
+        <div class="buttons">
+          <button class="remove">
+            <i class="fa fa-trash-alt"></i>
+          </button>
+          <button class="complete">
+            <i class="far fa-check-circle"></i>
+            <i class="fas fa-check-circle"></i>
+          </button>
+        </div>
+      </li>`
+    }
+  })
+  getELE("todo").innerHTML = todo;
+  getELE("completed").innerHTML = completed;
+}
